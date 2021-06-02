83c4b524c62482367d2bf3ceaa58206f6eacd0ed
Enio N. de Souza <enionsouza@gmail.com>
2021-06-02 19:04:52 -0400

    Add validation for uniqueness and add errors handling for edit view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
 
   validates :username, :email, presence: true
+  validates :username, :email, uniqueness: true
   validates :password, length: { in: 6..20 }, presence: true
 end
