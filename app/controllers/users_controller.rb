d59fda0b6f58b655991b2c83a5c178239afef66d
angelacuahutle <angela.cuahutle@gmail.com>
2021-06-02 15:25:29 -0500

    Define methof edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,9 +16,15 @@ class UsersController < ApplicationController
     end
   end
 
-  private
+  def edit 
+    @user = User.find(params[:id])
+  end 
 
+  
+
+private 
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
+
 end
